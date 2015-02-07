a940de9a26916fc24860a41a95cefef580eb8a13
kimena <kimena@plu.edu>
2015-02-06 21:07:16 -0800

    Added p(t) function in SEIRChain

--- BeOatmeal/src/SEIRChain.java
+++ BeOatmeal/src/SEIRChain.java
@@ -50,7 +50,7 @@ public class SEIRChain extends MarkovChain {
 		this.h = h;
 		this.beta = beta;
 		
-		this.p = 1 - Math.pow(Math.E, (-beta * h * i_0) / n);
+		setP();
 		
 		this._delt = 1/delt;
 		this.p_c = 1 - Math.pow(Math.E, -delt * h);
@@ -72,6 +72,10 @@ public class SEIRChain extends MarkovChain {
 	protected void iterate() throws Exception {
 		x_n.multL(a);
 		x_i.add(x_n.copy());
+	}
+	
+	private void setP() {
+		p = 1 - Math.pow(Math.E, (-beta * h * i) / n);
 	}
 	
 	private void setX_0() {
