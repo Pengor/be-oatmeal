4d98ec40f6515a72d2e54364dcbfa8c1ad86ce09
kimena <kimena@plu.edu>
2015-02-06 20:22:02 -0800

    Continued adding fields to SEIRChain

--- BeOatmeal/src/SEIRSeries.java
+++ BeOatmeal/src/SEIRSeries.java
@@ -1,13 +1,21 @@
 
 public class SEIRSeries extends MarkovChain {
 	
-	private double S;
-	private double E;
-	private double I;
-	private double R;
+	private double s;
+		private double s_0;
+	private double e;
+		private double e_0;
+	private double i;
+		private double i_0;
+	private double r;
+		private double r_0;
+	
+	private double n;
 	
 	private double h;
 	private double beta;
+	
+	private double p;
 	
 	private double _delt;
 	private double p_c;
