033f3e1fcb0230169e0ea1c5672d96ef28a7465c
Pengor <penguinemperor@hotmail.com>
2015-02-06 20:51:44 -0800

    minor changes to SEIRChain.java

--- BeOatmeal/src/SEIRChain.java
+++ BeOatmeal/src/SEIRChain.java
@@ -1,13 +1,13 @@
 
 public class SEIRChain extends MarkovChain {
 	
-	private double s;
+	private double s; // people susceptible
 		private double s_0;
-	private double e;
+	private double e; // people exposed
 		private double e_0;
-	private double i;
+	private double i; // people infectious
 		private double i_0;
-	private double r;
+	private double r; // people removed
 		private double r_0;
 	
 	private double n;
@@ -24,11 +24,25 @@ public class SEIRChain extends MarkovChain {
 	private double p_r;
 	
 	
+	/**
+	 * Constructor
+	 * @param a Matrix a, the stochastic matrix
+	 * @param x_0 Matrix x_0, the initial state vector
+	 */
 	public SEIRChain(Matrix a, Matrix x_0) {
 		super(a, x_0);
 	}
 	
-	
+
+	/**
+	 * @param n
+	 * @param s_0
+	 * @param e_0
+	 * @param i_0
+	 * @param beta
+	 * @param gam
+	 * @param delt
+	 */
 	public SEIRChain(double n, double s_0, double e_0, double i_0, double beta, double gam, double delt) {
 		super(null, null);
 	}
@@ -41,4 +55,6 @@ public class SEIRChain extends MarkovChain {
 		x_n.multL(a);
 		x_i.add(x_n.copy());
 	}
+	
+	
 }
