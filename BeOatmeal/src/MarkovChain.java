ea470322e3a921e72ac322a31be56d279379dd4c
kimena <kimena@plu.edu>
2015-02-06 20:16:21 -0800

    Started interate() in MarkovChain

--- BeOatmeal/src/MarkovChain.java
+++ BeOatmeal/src/MarkovChain.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 
 public class MarkovChain {
+	
 	protected Matrix a;
 	protected Matrix x_0;
 	
@@ -8,17 +9,28 @@ public class MarkovChain {
 	private ArrayList<Matrix> x_i;
 	
 	
+	/**
+	 * @param a
+	 * @param x_0
+	 */
 	public MarkovChain(Matrix a, Matrix x_0) {
 		this.a = a;
 		this.x_0 = x_0;
 		//Fix references and copy instead
-		this.x_n = x_0;
+		this.x_n = x_0.copy();
 		
 		x_i = new ArrayList<Matrix>();
 		x_i.add(this.x_0);
 	}
 	
-	protected void iterate() {
-		//a.multR(x_n);
+	
+	
+	/**
+	 * @throws Exception
+	 */
+	protected void iterate() throws Exception {
+		x_n.multL(a);
+		x_i.add(x_n.copy());
+		
 	}
 }
